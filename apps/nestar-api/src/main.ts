d1acfda3217f212b4544e4a6f396bc9869ab27f6
Azizbek Siddikov <azbeksid@gmail.com>
2025-03-09 14:54:54 +0900

    feat: integrate pipe validation logic

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_API ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT_API ?? 3000);
+	console.log(`started at http://localhost:${process.env.PORT_API ?? 3000}`);
 }
 bootstrap();
