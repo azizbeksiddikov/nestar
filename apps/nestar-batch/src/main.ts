7c188888b079cf87bc643f77c825ef96f2620bad
Azizbek Siddikov <azbeksid@gmail.com>
2025-04-06 17:54:33 +0900

    fix: modify batch module and connect to MongoDB

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { NestarBatchModule } from './nestar-batch.module';
+import { BatchModule } from './batch.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 3000);
+	const app = await NestFactory.create(BatchModule);
+	await app.listen(process.env.PORT_BATCH ?? 3000);
+
+	console.log(`Batch server is running on http://localhost:${process.env.PORT_BATCH ?? 3000}\n`);
 }
 bootstrap();
