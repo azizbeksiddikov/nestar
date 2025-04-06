148e827c09bf57eaf647167a052aa0b304219895
Azizbek Siddikov <azbeksid@gmail.com>
2025-04-06 17:28:49 +0900

    feat: create WebSocket Connection process

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -14,11 +15,10 @@ async function bootstrap() {
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
 	app.use('/uploads', express.static('./uploads'));
 
+	app.useWebSocketAdapter(new WsAdapter(app));
+
 	await app.listen(process.env.PORT_API ?? 3000);
 
 	console.log(`started at http://localhost:${process.env.PORT_API ?? 3000}\n`);
 }
 bootstrap();
-
-// Are imports in module/imports happening synchronously?
-// Why do we need batch project?
